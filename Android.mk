525244912e5dc9351ad75bcded78ce72fe666b37
Jay Shrauner <shrauner@google.com>
2013-08-06 14:33:07 -0700

    Remove GooglePhone
    Remove GooglePhone
    
    Bug:
    Change-Id: I6b0f4c68bd617422d915979112ad85bab6aa8a98

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@ LOCAL_SRC_FILES += \
         src/com/android/phone/INetworkQueryServiceCallback.aidl
 
 LOCAL_PACKAGE_NAME := TeleService
-LOCAL_OVERRIDES_PACKAGES := Phone GooglePhone
+LOCAL_OVERRIDES_PACKAGES := Phone
 
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
