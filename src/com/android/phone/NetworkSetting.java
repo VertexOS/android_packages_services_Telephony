0930eacde371116ed4b55a625d086a52b45894ec
Richard LIU <richardl@codeaurora.org>
2013-12-20 14:43:33 +0800

    Display the radio tech in manually PLMN search results
    Display 2G/3G/4G suffix in the operator list to distinguish
    different networks of the same operator.
    
    CRs-Fixed: 522564
    Change-Id: I007d1abcfba1532e94de712af9d76bf5099c193f

--- src/com/android/phone/NetworkSetting.java
+++ src/com/android/phone/NetworkSetting.java
@@ -77,6 +77,10 @@ public class NetworkSetting extends PreferenceActivity
     private HashMap<Preference, OperatorInfo> mNetworkMap;
 
     int mPhoneId = SubscriptionManager.INVALID_PHONE_INDEX;
+
+    //map of RAT type values to user understandable strings
+    private HashMap<String, String> mRatMap;
+
     protected boolean mIsForeground = false;
 
     private UserManager mUm;
@@ -262,6 +266,8 @@ public class NetworkSetting extends PreferenceActivity
 
         mNetworkList = (PreferenceGroup) getPreferenceScreen().findPreference(LIST_NETWORKS_KEY);
         mNetworkMap = new HashMap<Preference, OperatorInfo>();
+        mRatMap = new HashMap<String, String>();
+        initRatMap();
 
         mSearchButton = getPreferenceScreen().findPreference(BUTTON_SRCH_NETWRKS_KEY);
         mAutoSelect = getPreferenceScreen().findPreference(BUTTON_AUTO_SELECT_KEY);
@@ -509,14 +515,20 @@ public class NetworkSetting extends PreferenceActivity
      */
 
     private String getNetworkTitle(OperatorInfo ni) {
+        String title;
+
         if (!TextUtils.isEmpty(ni.getOperatorAlphaLong())) {
-            return ni.getOperatorAlphaLong();
+            title = ni.getOperatorAlphaLong();
         } else if (!TextUtils.isEmpty(ni.getOperatorAlphaShort())) {
-            return ni.getOperatorAlphaShort();
+            title = ni.getOperatorAlphaShort();
         } else {
             BidiFormatter bidiFormatter = BidiFormatter.getInstance();
-            return bidiFormatter.unicodeWrap(ni.getOperatorNumeric(), TextDirectionHeuristics.LTR);
+            title = bidiFormatter.unicodeWrap(ni.getOperatorNumeric(), TextDirectionHeuristics.LTR);
         }
+        if (!ni.getRadioTech().equals(""))
+            title += " " + mRatMap.get(ni.getRadioTech());
+
+        return title;
     }
 
     private void clearList() {
@@ -537,6 +549,27 @@ public class NetworkSetting extends PreferenceActivity
         if (phone != null) {
             phone.setNetworkSelectionModeAutomatic(msg);
         }
+    }
+
+    private void initRatMap() {
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_UNKNOWN), "Unknown");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_GPRS), "2G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_EDGE), "2G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_UMTS), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_IS95A), "2G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_IS95B), "2G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_1xRTT), "2G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_EVDO_0), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_EVDO_A), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_HSDPA), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_HSUPA), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_HSPA), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_EVDO_B), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_EHRPD), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_LTE), "4G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_HSPAP), "3G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_GSM), "2G");
+        mRatMap.put(String.valueOf(ServiceState.RIL_RADIO_TECHNOLOGY_TD_SCDMA), "3G");
     }
 
     private void log(String msg) {
