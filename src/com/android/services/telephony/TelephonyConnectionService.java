c2d0e8c8aaac5ec4ad394c5824f6c3265f751977
Nancy Chen <nancychen@google.com>
2014-09-05 20:08:34 -0700

    getHandle -> getAddress (ConnectionRequest).
    Made changes per API review.
    
    Bug: 17329632
    Change-Id: I6f2043d6293f3a369c4f4ecfca996ae2061634d2

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -67,7 +67,7 @@ public class TelephonyConnectionService extends ConnectionService {
             final ConnectionRequest request) {
         Log.v(this, "onCreateOutgoingConnection, request: " + request);
 
-        Uri handle = request.getHandle();
+        Uri handle = request.getAddress();
         if (handle == null) {
             Log.d(this, "onCreateOutgoingConnection, handle is null");
             return Connection.createFailedConnection(DisconnectCause.NO_PHONE_NUMBER_SUPPLIED,
