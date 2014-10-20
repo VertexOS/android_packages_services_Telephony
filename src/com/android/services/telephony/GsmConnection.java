c4194d35d4638595a6be0c10f8e7c623cccfd09a
Andrew Lee <anwlee@google.com>
2014-10-20 14:43:45 -0700

    Set ADD_CALL capabilities on TelephonyConnections.
    Before this was done purely in Telecom's InCallController. But, IMS
    now adds some wrinkles on the situations where this is possible. Now,
    specify this on the connection, and remove on the InCallController if
    the capability is not appropriate.
    
    This allows for greater control over ADD_CALL capability, depending
    on the type/state of the TelephonyConnection.
    
    Bug: 17423848
    Change-Id: I629b87e1652ddfd7008f56e9b46669b9343232db

--- src/com/android/services/telephony/GsmConnection.java
+++ src/com/android/services/telephony/GsmConnection.java
@@ -65,6 +65,7 @@ final class GsmConnection extends TelephonyConnection {
     @Override
     protected int buildCallCapabilities() {
         int capabilities = super.buildCallCapabilities();
+        capabilities |= PhoneCapabilities.ADD_CALL;
         capabilities |= PhoneCapabilities.MUTE;
         capabilities |= PhoneCapabilities.SUPPORT_HOLD;
         if (getState() == STATE_ACTIVE || getState() == STATE_HOLDING) {
