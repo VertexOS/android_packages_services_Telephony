253c8f9c07d9d9e8ed192cb9247cef97dc04f032
Nancy Chen <nancychen@google.com>
2014-11-04 12:25:49 -0800

    Set default color of single-SIM PhoneAccount to NO_COLOR.
    The default SIM of a Single-SIM phones should not have a color so that
    the InCallUI uses the default theme color.
    
    Bug: 18239553
    Change-Id: I042918d6f4756e39339e18b4cb65ec83c09805fb

--- src/com/android/services/telephony/TelecomAccountRegistry.java
+++ src/com/android/services/telephony/TelecomAccountRegistry.java
@@ -103,7 +103,7 @@ final class TelecomAccountRegistry {
 
             // Populate the phone account data.
             int subId = mPhone.getSubId();
-            int color = 0;
+            int color = PhoneAccount.NO_COLOR;
             int slotId = SubscriptionManager.INVALID_SLOT_ID;
             String line1Number = mTelephonyManager.getLine1NumberForSubscriber(subId);
             if (line1Number == null) {
