2a2211f46c7b4814ee421fc592b6f542e5c62a3c
Tyler Gunn <tgunn@google.com>
2015-04-20 14:16:36 -0700

    Renaming IConferenceable to Conferenceable.
    Bug: 20165255
    Change-Id: I63918f8d3bb9be5dfc1db831669511dfc7348946

--- src/com/android/services/telephony/ImsConferenceController.java
+++ src/com/android/services/telephony/ImsConferenceController.java
@@ -21,7 +21,7 @@ import android.telecom.Conference;
 import android.telecom.Connection;
 import android.telecom.ConnectionService;
 import android.telecom.DisconnectCause;
-import android.telecom.IConferenceable;
+import android.telecom.Conferenceable;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -146,8 +146,8 @@ public class ImsConferenceController {
      */
     private void recalculateConferenceable() {
         Log.v(this, "recalculateConferenceable : %d", mTelephonyConnections.size());
-        List<IConferenceable> activeConnections = new ArrayList<>(mTelephonyConnections.size());
-        List<IConferenceable> backgroundConnections = new ArrayList<>(mTelephonyConnections.size());
+        List<Conferenceable> activeConnections = new ArrayList<>(mTelephonyConnections.size());
+        List<Conferenceable> backgroundConnections = new ArrayList<>(mTelephonyConnections.size());
 
         // Loop through and collect all calls which are active or holding
         for (Connection connection : mTelephonyConnections) {
@@ -190,7 +190,7 @@ public class ImsConferenceController {
 
         // Go through all the active connections and set the background connections as
         // conferenceable.
-        for (IConferenceable conferenceable : activeConnections) {
+        for (Conferenceable conferenceable : activeConnections) {
             if (conferenceable instanceof Connection) {
                 Connection connection = (Connection) conferenceable;
                 connection.setConferenceables(backgroundConnections);
@@ -199,7 +199,7 @@ public class ImsConferenceController {
 
         // Go through all the background connections and set the active connections as
         // conferenceable.
-        for (IConferenceable conferenceable : backgroundConnections) {
+        for (Conferenceable conferenceable : backgroundConnections) {
             if (conferenceable instanceof Connection) {
                 Connection connection = (Connection) conferenceable;
                 connection.setConferenceables(activeConnections);
