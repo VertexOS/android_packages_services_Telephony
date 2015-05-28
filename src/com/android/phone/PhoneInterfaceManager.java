8377581717140c62101e45b9943aa50e46fdaae3
Etan Cohen <etancohen@google.com>
2015-05-28 15:57:13 -0700

    Rename VoLTE/WFC enable API to available - add VT available API.
    Renamed API to use consistent semantics: available refers to the current
    state of the service rather then whether feature is enabled by device/carrier/
    user.
    
    Added VT API to match VoLTE/WFC.
    
    Change-Id: Ia2ed60bc88c6ac58e6cdab7e2eb31d383440c949

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -2352,7 +2352,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      * {@hide}
      * Returns the IMS Registration Status
      */
-    public boolean isWifiCallingEnabled() {
+    public boolean isWifiCallingAvailable() {
         return mPhone.isWifiCallingEnabled();
     }
 
@@ -2360,8 +2360,15 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      * {@hide}
      * Returns the IMS Registration Status
      */
-    public boolean isVolteEnabled() {
+    public boolean isVolteAvailable() {
         return mPhone.isVolteEnabled();
+    }
+
+    /*
+     * {@hide} Returns the IMS Registration Status
+     */
+    public boolean isVideoTelephonyAvailable() {
+        return mPhone.isVideoEnabled();
     }
 
     private boolean canReadPhoneState(String callingPackage, String message) {
