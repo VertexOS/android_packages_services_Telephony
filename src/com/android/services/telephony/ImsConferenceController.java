047fa54406b4b46b651101f40a67588472bac979
Anju Mathapati <anjucm@codeaurora.org>
2015-06-16 18:02:11 +0530

    IMS: Merge calls option not seen when refer failed
    Recalculate the conferenceable connections once conference call
    is started. This will ensure conferenceable connections are updated
    correctly even when refer failed for one of the calls.
    
    BUG=24537276
    Change-Id: I40355fbb6fb7d82edd48b5ebb3bdb6b6b09406bf

--- src/com/android/services/telephony/ImsConferenceController.java
+++ src/com/android/services/telephony/ImsConferenceController.java
@@ -75,7 +75,7 @@ public class ImsConferenceController {
         @Override
         public void onConferenceStarted() {
             Log.v(this, "onConferenceStarted");
-            recalculateConference();
+            recalculate();
         }
     };
 
