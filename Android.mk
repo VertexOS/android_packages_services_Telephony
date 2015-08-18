b018a8f16ffea52debf385b921c7dc51379522db
Jay Shrauner <shrauner@google.com>
2015-08-18 12:52:31 -0700

    Remove com.android.services.telephony.common
    Move MoreStrings to Dialer/util. AudioMode was unused.
    
    Bug:
    Change-Id: I02e0e1c6dfeeec621e341f2f59df74df0e7e18f0

--- Android.mk
+++ Android.mk
@@ -11,8 +11,7 @@ res_dirs := res $(phone_common_dir)/res sip/res
 
 LOCAL_JAVA_LIBRARIES := telephony-common voip-common ims-common
 LOCAL_STATIC_JAVA_LIBRARIES := \
-        com.android.services.telephony.common \
-        guava \
+        guava
 
 LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs))
 LOCAL_SRC_FILES += \
