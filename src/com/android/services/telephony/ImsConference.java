80e5718cf33451905408956838a98835e3e2441f
Amit Mahajan <amitmahajan@google.com>
2015-12-16 10:41:34 -0800

    Added null checks to avoid NPEs.
    Bug: 25793157
    Change-Id: I1fec5855e59e8ef1986d6d0125953ca199c6476f

--- src/com/android/services/telephony/ImsConference.java
+++ src/com/android/services/telephony/ImsConference.java
@@ -475,7 +475,8 @@ public class ImsConference extends Conference {
         com.android.internal.telephony.Connection originalConnection =
                 mConferenceHost.getOriginalConnection();
 
-        return originalConnection.isMultiparty() && originalConnection.isConferenceHost();
+        return originalConnection != null && originalConnection.isMultiparty() &&
+                originalConnection.isConferenceHost();
     }
 
     /**
@@ -763,7 +764,8 @@ public class ImsConference extends Conference {
         com.android.internal.telephony.Connection originalConnection =
                 mConferenceHost.getOriginalConnection();
 
-        if (originalConnection.getPhoneType() != PhoneConstants.PHONE_TYPE_IMS) {
+        if (originalConnection != null &&
+                originalConnection.getPhoneType() != PhoneConstants.PHONE_TYPE_IMS) {
             if (Log.VERBOSE) {
                 Log.v(this,
                         "Original connection for conference host is no longer an IMS connection; " +
