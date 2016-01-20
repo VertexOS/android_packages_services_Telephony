a3f29e48cfc2634e1e192712535832f0cba9e70a
Zhu Youhua <youhua@codeaurora.org>
2016-01-20 15:14:44 +0800

    TeleService: Add a controller for proximity sensor
    Add the setting for enabling proximity sensor or not when calling
    
    Change-Id: I06807c37f2bb4a50e0e3051f20305549ab47e846
    CRs-Fixed: 914710

--- src/com/android/phone/Constants.java
+++ src/com/android/phone/Constants.java
@@ -134,6 +134,11 @@ public class Constants {
      */
     public static final String SETTINGS_VIBRATE_WHEN_ACCEPTED = "vibrate_on_accepted";
 
+    /**
+     * enable proximity sensor or not. 0 = Off 1 = On
+     */
+    public static final String SETTINGS_PROXIMITY_SENSOR = "proximity_sensor";
+
     //
     // TODO: Move all the various EXTRA_* and intent action constants here too.
     // (Currently they're all over the place: InCallScreen,
