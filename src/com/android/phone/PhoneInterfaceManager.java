f76543a08a3d12e2044137cd6bd05df6bc6670c9
Qiang Chen <cqiang@codeaurora.org>
2016-06-29 18:38:31 +0800

    NetworkSettingsReset: Reset to correct mode when network seetings reset
    Reset to correct mode when network seetings reset for some carriers
    
    CRs-Fixed: 1030601
    
    Change-Id: I700154ac0a83d8f65dfe2dde6a2984a4f8a7f6d4

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -34,6 +34,7 @@ import android.os.Message;
 import android.os.Process;
 import android.os.ResultReceiver;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.PreferenceManager;
@@ -141,6 +142,8 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     private static final int CMD_SIM_GET_ATR = 43;
     private static final int EVENT_SIM_GET_ATR_DONE = 44;
     private static final int CMD_OPEN_CHANNEL_WITH_P2 = 45;
+
+    private static final String PRIMARY_CARD_PROPERTY_NAME = "persist.radio.primarycard";
 
     /** The singleton instance. */
     private static PhoneInterfaceManager sInstance;
@@ -2970,6 +2973,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         }
 
         final long identity = Binder.clearCallingIdentity();
+        int networkMode;
         try {
             if (SubscriptionManager.isUsableSubIdValue(subId) && !mUserManager.hasUserRestriction(
                     UserManager.DISALLOW_CONFIG_MOBILE_NETWORKS)) {
@@ -2978,7 +2982,14 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                 // Set network selection mode to automatic
                 setNetworkSelectionModeAutomatic(subId);
                 // Set preferred mobile network type to the best available
-                setPreferredNetworkType(subId, Phone.PREFERRED_NT_MODE);
+                if (SystemProperties.getBoolean(PRIMARY_CARD_PROPERTY_NAME, false) ) {
+                    networkMode = PhoneFactory.calculatePreferredNetworkType(
+                            mPhone.getContext(), subId);
+                } else {
+                    networkMode = Phone.PREFERRED_NT_MODE;
+                }
+                setPreferredNetworkType(subId, networkMode);
+
                 // Turn off roaming
                 SubscriptionManager.from(mApp).setDataRoaming(0, subId);
             }
