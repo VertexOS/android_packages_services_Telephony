lt03lte-cafaosp/packages-services-Telephony

IMS: Handle conference support information related extra
During an ims call, when calculating the add participant
capability, consider the conference support information
related extra.

Change-Id: I4d578e28936dc0b5847555dd7efcc1159ad286e4
CRs-Fixed: 1069442

src/com/android/services/telephony/TelephonyConnection.java
@@ -64,6 +64,8 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.codeaurora.ims.QtiCallConstants;
+
 /**
  * Base class for CDMA and GSM connections.
  */
@@ -1084,6 +1086,14 @@ abstract class TelephonyConnection extends Connection {
                     }
                     connectionExtras.putAll(mOriginalConnectionExtras);
                     setExtras(connectionExtras);
+
+                    // If extras contain Conference support information,
+                    // then ensure capabilities are updated and propagated to Telecom.
+                    if (connectionExtras.containsKey(
+                            QtiCallConstants.CONF_SUPPORT_IND_EXTRA_KEY)) {
+                        updateConnectionCapabilities();
+                    }
+
                 } else {
                     Log.d(this, "Extras update not required");
                 }
@@ -1408,10 +1418,22 @@ abstract class TelephonyConnection extends Connection {
     }
 
     private boolean isAddParticipantCapable() {
-        return getPhone() != null &&
+        boolean isCapable = getPhone() != null &&
                (getPhone().getPhoneType() == PhoneConstants.PHONE_TYPE_IMS) &&
                !mIsEmergencyNumber && (mOriginalConnectionState == Call.State.ACTIVE
                || mOriginalConnectionState == Call.State.HOLDING);
+
+        /**
+         * For individual IMS calls, if the extra for remote conference support is
+         *     - indicated, then consider the same for add participant capability
+         *     - not indicated, then the add participant capability is same as before.
+         */
+        if (isCapable && (mOriginalConnection != null) && !mIsMultiParty) {
+            isCapable = mOriginalConnectionExtras.getBoolean(
+                    QtiCallConstants.CONF_SUPPORT_IND_EXTRA_KEY, true);
+            Log.i(this, "isAddParticipantCapable: lower layer indication=" + isCapable);
+        }
+        return isCapable;
     }
 
     private int applyCapability(int capabilities, int capability) {

VertexOS/android_packages_services_Telephony

src/com/android/services/telephony/TelephonyConnection.java
@@ -71,6 +71,8 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.codeaurora.ims.QtiCallConstants;
+
 /**
  * Base class for CDMA and GSM connections.
  */
@@ -1451,6 +1453,14 @@ abstract class TelephonyConnection extends Connection {
 
                     // Ensure extras are propagated to Telecom.
                     putExtras(mOriginalConnectionExtras);
+
+                    // If extras contain Conference support information,
+                    // then ensure capabilities are updated and propagated to Telecom.
+                    if (mOriginalConnectionExtras.containsKey(
+                            QtiCallConstants.CONF_SUPPORT_IND_EXTRA_KEY)) {
+                        updateConnectionCapabilities();
+                    }
+
                 } else {
                     Log.d(this, "Extras update not required");
                 }
@@ -1814,10 +1824,22 @@ abstract class TelephonyConnection extends Connection {
     }
 
     private boolean isAddParticipantCapable() {
-        return getPhone() != null &&
+        boolean isCapable = getPhone() != null &&
                (getPhone().getPhoneType() == PhoneConstants.PHONE_TYPE_IMS) &&
                !mIsEmergencyNumber && (mConnectionState == Call.State.ACTIVE
                || mConnectionState == Call.State.HOLDING);
+
+        /**
+         * For individual IMS calls, if the extra for remote conference support is
+         *     - indicated, then consider the same for add participant capability
+         *     - not indicated, then the add participant capability is same as before.
+         */
+        if (isCapable && (mOriginalConnection != null) && !mIsMultiParty) {
+            isCapable = mOriginalConnectionExtras.getBoolean(
+                    QtiCallConstants.CONF_SUPPORT_IND_EXTRA_KEY, true);
+            Log.i(this, "isAddParticipantCapable: lower layer indication=" + isCapable);
+        }
+        return isCapable;
     }
 
     private int applyCapability(int capabilities, int capability) {